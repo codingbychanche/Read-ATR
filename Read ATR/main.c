47c143a8f6d79bb85db44d30540e819fbe83eeed
Berthold Fritz <Berthold@bertholds-apple-ii.fritz.box>
2014-03-18 11:39:35 +0100

    Initial Commit

---
+++ Read ATR/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Read ATR
+//
+//  Created by Berthold Fritz on 18.03.14.
+//  Copyright (c) 2014 Kappler Mensch & Schneider. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
